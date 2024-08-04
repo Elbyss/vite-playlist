0768f5f20409ce74b310304b2d8ae16f4c1fdd54
Elbyss <elysiabyss@gmail.com>
2024-08-04 10:48:40 +0900

    modify tailwind config screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      lg: { max: '1023px' },
+      md: { max: '767px' },
+      sm: { max: '639px' },
+    },
   },
   plugins: [],
 };
