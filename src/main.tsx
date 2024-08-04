879d6ee8e8230710a5c9f1520706d5643c2c3e91
Elbyss <elysiabyss@gmail.com>
2024-08-04 12:40:30 +0900

    add toast msg

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import { routes } from './routes';
 import { RecoilRoot } from 'recoil';
+import { Toaster } from 'sonner';
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
+      <Toaster position='top-left' expand={true} richColors />
       <RouterProvider router={router} />
     </RecoilRoot>
   </React.StrictMode>
